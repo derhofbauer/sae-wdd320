50dbd4eb9259a5e1e9dbe1686d9040dfd70da844
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2021-05-04 17:35:57 +0200

    [FIX] Category List

--- mvc/resources/views/templates/categories/index.php
+++ mvc/resources/views/templates/categories/index.php
@@ -1,8 +1,7 @@
-<div class="categories">
-    <article>
-        <h2><?php echo $post->title; ?></h2>
-        <?php require __DIR__ . '/../../partials/post/meta.php'; ?>
-
-        <div class="content"><?php echo $post->content; ?></div>
+<?php foreach($categories as $category): ?>
+    <article class="category">
+        <h2><?php echo $category->title; ?></h2>
+        <div class="content"><?php echo $category->description; ?></div>
+        <a href="categories/<?php echo $category->slug; ?>" class="more">Show Posts</a>
     </article>
-</div>
+<?php endforeach; ?>
